c8f63c7a1bd71f67e2ab45f9a3f9e51f9bec03f2
Pedro <50713178+prgf87@users.noreply.github.com>
2022-07-13 19:18:55 -0400

    Load Products from MongoDB + updateCartHandl.er

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,50 @@
-import data from '../utils/data';
+import { useContext } from 'react';
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
+import Product from '../models/Product';
+import db from '../utils/db';
+import { Store } from '../utils/Store';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-export default function Home() {
+export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry, the product is not available');
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product has been added to your cart');
+  };
+
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
-          <ProductItem product={product} key={product.slug}></ProductItem>
+        {products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          ></ProductItem>
         ))}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }

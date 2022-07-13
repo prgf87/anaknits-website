7b3646c951b4c1add8a4a01bb40576f4f87bf138
Pedro <50713178+prgf87@users.noreply.github.com>
2022-07-13 16:06:15 -0400

    Payment Screen implemented

--- utils/Store.js
+++ utils/Store.js
@@ -51,7 +51,14 @@ function reducer(state, action) {
           },
         },
       };
-
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      };
     default:
       return state;
   }

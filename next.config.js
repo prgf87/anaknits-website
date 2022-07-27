a8b0b2d01e078a31deff2f548eed81f0813231bd
Pedro <50713178+prgf87@users.noreply.github.com>
2022-07-27 16:52:44 -0400

    CORS implemented

--- next.config.js
+++ next.config.js
@@ -7,4 +7,28 @@ const nextConfig = {
   },
 };
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'anaknits.com' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;

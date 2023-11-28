f6c551c583b76146e1353cbaebb6c1a2c46fc21a
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-28 21:55:56 +0000

    comment out api

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,12 +1,12 @@
 // import Product from '../../models/Product';
-import User from '../../models/User';
-import data from '../../utils/dataNew';
+// import User from '../../models/User';
+// import data from '../../utils/dataNew';
 import db from '../../utils/db';
 
 const handler = async (req, res) => {
   await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
+  // await User.deleteMany();
+  // await User.insertMany(data.users);
   // await Product.deleteMany();
   // await Product.insertMany(data.products);
   await db.disconnect();

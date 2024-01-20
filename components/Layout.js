d97ffafc7a8dc21f1aec99bc888242216ae5c315
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-20 19:00:20 +0000

    removes unused code in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,37 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { getError } from '../utils/error';
-import axios from 'axios';
-import Footer from './footer';
-import Navbar from './Navbar';
+import React from "react";
+import Head from "next/head";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Footer from "./footer";
+import Navbar from "./Navbar";
 
 function Layout({ title, children }) {
-  // eslint-disable-next-line no-unused-vars
-  const [categories, setCategories] = useState([]);
-  const fetchCategories = async () => {
-    try {
-      const { data } = await axios.get(`/api/products/categories`);
-
-      setCategories(data);
-    } catch (err) {
-      toast.error(getError(err));
-    }
-  };
-  useEffect(() => {
-    fetchCategories();
-  }, []);
-
   return (
     <>
       <Head>
-        <title>{title ? title + ' - Anaknits' : 'Anaknits'}</title>
+        <title>{title ? title + " - Anaknits" : "Anaknits"}</title>
         <meta name="home page" content="Anaknits Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <ToastContainer position="bottom-center" limit={1} />
+      <ToastContainer position="top-center" limit={1} />
 
       <>
         <header>

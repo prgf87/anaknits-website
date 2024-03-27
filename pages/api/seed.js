b92cca45ebfc43c132dc07bf6346d4ad5cfbb490
Pedro <50713178+prgf87@users.noreply.github.com>
2024-03-27 15:00:52 +0000

    products and orders deleted - new categories added

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,7 +1,8 @@
-// import Product from '../../models/Product';
+// import Product from "../../models/Product";
 // import User from '../../models/User';
 // import data from '../../utils/dataNew';
-import db from '../../utils/db';
+// import Order from "../../models/Order";
+import db from "../../utils/db";
 
 const handler = async (req, res) => {
   await db.connect();
@@ -9,7 +10,8 @@ const handler = async (req, res) => {
   // await User.insertMany(data.users);
   // await Product.deleteMany();
   // await Product.insertMany(data.products);
+  // await Order.deleteMany();
   await db.disconnect();
-  res.send({ message: 'Seeded successfully' });
+  res.send({ message: "Seeded successfully" });
 };
 export default handler;

5e9820b0da14dac6c81d917901183fbbde3bec0f
Pedro <prgf2011@gmail.com>
2024-04-10 17:39:24 +0100

    adds product details to model/api

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -9,6 +9,7 @@ export const customParams = {
 };
 
 export default function ProductItem({ product, addToCartHandler }) {
+  console.log(product);
   return (
     <div className="card mx-0 md:mx-2">
       <Link href={`/products/${product.slug}`}>
@@ -23,20 +24,20 @@ export default function ProductItem({ product, addToCartHandler }) {
           className="rounded-t shadow-md object-cover w-full"
         />
       </Link>
-      <div className="flex flex-col items-center justify-between py-1 md:py-2 lg:py-4 px-2 text-sm text-center">
+      <div className="flex flex-col items-center justify-evenly py-1 md:py-2 lg:py-4 px-4 text-sm text-center">
         <h2 className="font-bold pt-2 px-1 text-ellipsis">{product.name}</h2>
-        <div className="flex-col w-full text-xs pt-2">
-          <h3 className="font-semibold">
+        <p className="font-light py-3 px-1 text-ellipsis">{product.details}</p>
+        <div className="flex-col w-full text-xs py-1">
+          <p className="font-semibold">
             Available in{" "}
             <span className="border rounded-full py-[2px] px-[6px]">
               {product.colours.length}
             </span>
             &nbsp;colours
-          </h3>
+          </p>
         </div>
         <p className="font-bold p-2">${product.price.toFixed(2)}</p>
-
-        <div className="relative">
+        <div className="relative w-full px-4">
           <button
             className="primary-button text-sm mb-2"
             type="button"
